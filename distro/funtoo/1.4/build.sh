2a709909033d4ad576ea1756a6d2cc0cb3af6bb6
Daniele Rondina <geaaru@sabayonlinux.org>
2020-06-06 17:20:05 +0200

    distro/funtoo: Bump v.1.4+2020.04.10

--- distro/funtoo/1.4/build.sh
+++ distro/funtoo/1.4/build.sh
@@ -2,7 +2,11 @@
 set -ex
 apk --no-cache add ca-certificates tar wget xz rsync
 
-wget https://build.funtoo.org/1.4-release-std/arm-32bit/raspi3/2020-01-03/stage3-raspi3-1.4-release-std-2020-01-03.tar.xz -O /rootfs.tar.xz
+FUNTOO_RELEASE="1.4"
+DATE_VERSION=${DATE_VERSION:-$(echo "${PACKAGE_VERSION}" | cut -d'+' -f 2)}
+
+wget https://build.funtoo.org/${FUNTOO_RELEASE}-release-std/arm-32bit/raspi3/${DATE_VERSION}/stage3-raspi3-${FUNTOO_RELEASE}-release-std-${DATE_VERSION}.tar.xz -O /rootfs.tar.xz
+
 mkdir /temp && cd /temp && tar -xJf /rootfs.tar.xz --xattrs --numeric-owner && rm /rootfs.tar.xz
 rsync -A -a --delete --numeric-ids --recursive -d -H --one-file-system --xattrs --exclude '/temp/*'  --exclude '/etc/resolv.conf'  --exclude '/etc/hostname'  --exclude '/sys/' --exclude '/etc/hosts'  --exclude '/sys/*' --exclude '/proc/*' --exclude '/dev/pts/*' /temp/ /
 rm -rf /temp
